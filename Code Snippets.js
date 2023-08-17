0a069df8afd012a3488a5fb7ba2c5278b6a77239
oconnesjGF <samuel.oconnell@glidefast.com>
2023-08-17 11:57:44 -0400

    Update Code Snippets.js
    added new function

--- Code Snippets.js
+++ Code Snippets.js
@@ -6,6 +6,14 @@ function findDifference(array1, array2) {
   });
 }
 
+function findMatches(array1, array2) {
+  // this compares two arrays and will find the elements that exist in both arrays.
+  //replace array1 and array2 values with the names of the arrys to pass in.
+  return array1.filter(function (element) {
+    return array2.indexOf(element) === 1;
+  });
+}
+
 
 function stringify(string) {
   //this function is for quickly converting JSON to a string.
